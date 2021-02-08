9208375152bf88f5554ff70c8436575577ebf866
Luca Salazar <43450396+lucasalazar@users.noreply.github.com>
2021-02-08 15:21:00 -0300

    Update script.js

--- scripts.js
+++ scripts.js
@@ -100,8 +100,8 @@ const Utils = {
         return (signal + value)
     },
     formatAmount(value){
-        value = Number(value) * 100
-        return value
+        value = value * 100
+        return Math.round(value)
     },
     formatDate(date){
         const splittedDate = date.split("-")
